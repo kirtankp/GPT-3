1408ccca2a009f7f89193dc22ab74c453728b93f
kirtan <kirtan1902@gmail.com>
2023-02-16 21:13:14 +0530

    completed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import {RiMenu3Line,RiCloseLine} from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+import './navbar.css'
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className='app__navbar'>
+      <div className='app__navbar-links'>
+        <div className='app__navabr-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+        <div className="app__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#app1">What is App1?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className='app__navbar-sign'>
+          <p>Sign in</p>
+          <button type='button'>Sign up</button>
+      </div>
+      <div className="app__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="app__navbar-menu_container scale-up-center">
+          <div className="app__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="app__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
